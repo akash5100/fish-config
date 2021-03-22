9d5d4a3222a8ea6574cebf79348b9dd8927c4d9c
akash5100 <shivamvermashs@gmail.com>
2021-03-22 11:05:00 +0530

    updated

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
-neofetch --ascii_distro arch --disable WM Themes Theme Icons DE Resolution packages shell gpu cpu --config off --bold off --colors 6 8 5 1 7 3 | lolcat
-figlet -f Univers  "        akash " | lolcat     
+neofetch --ascii_distro arch --disable WM Themes Theme Icons DE Resolution packages shell gpu cpu --config off --bold off --colors 1 5 7 4 7 5 --ascii_colors 5 5 --color_blocks off 
+figlet -f Slant "        akash5100 " | lolcat
 alias ls='lsd'
 alias lst='ls --tree'
 alias lsl='ls -l'
